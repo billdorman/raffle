b39fb0f3114bd65d10ecd36bf45fddd45a7ec646
Ransomware <null@ransomware.run>
2020-11-09 22:49:03 -0600

    fixing shit

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pymysql 
 import pymysql.cursors 
 import json 
- 
+
 db_host = 'localhost'
 db_user = 'lanadmin'
 db_pass = 'acorn77tog'
 db_name = 'raffle'
 
+app = Flask(__name__)
+
 conn = pymysql.connect(host='localhost', port=3306, user='lanadmin', passwd='acorn77tog', db='raffle')
-cur = conn.cursor()
-cur.execute("select * from items;")
-for row in cur:
-    # print(row)
-    row = json.dumps(row, indent=4, sort_keys=True, default=str)
 
-# jsonitems = json.dump(cur.description)
+@app.route('/')
+    cur = conn.cursor()
+    cur.execute("select * from items;")
+    for row in cur:
+        # print(row)
+        row = json.dumps(row, indent=4, sort_keys=True, default=str)
+        return(row)
 
-cur.close()
-conn.close()
+    # jsonitems = json.dump(cur.description)
+
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+   app.run(debug=True)
