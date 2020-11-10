fd2b06ff994b919ffd958b56d58cf6c308b57f40
Ransomware <null@ransomware.run>
2020-11-09 22:30:24 -0600

    Incrimental

--- main.py
+++ main.py
@@ -7,20 +7,13 @@ db_user = 'lanadmin'
 db_pass = 'acorn77tog'
 db_name = 'raffle'
 
-item_list = ''
-
 conn = pymysql.connect(host='localhost', port=3306, user='lanadmin', passwd='acorn77tog', db='raffle')
 cur = conn.cursor()
 cur.execute("select * from items;")
-print(cur.description)
-print()
-
 for row in cur:
     print(row)
 
-jsonitems = json.dump(cur.description)
+# jsonitems = json.dump(cur.description)
 
 cur.close()
 conn.close()
-
-#        json.dump(rows, write_file)
