64467ad5a4847bfdfb61b3b370de7c35f5988259
Ransomware <null@ransomware.run>
2020-11-09 23:05:48 -0600

    fixing shit

--- service.py
+++ service.py
@@ -12,10 +12,38 @@ db_name = 'raffle'
 
 conn = pymysql.connect(host='localhost', port=3306, user='lanadmin', passwd='acorn77tog', db='raffle')
 
-@app.route('/')
+@app.route('/users')
+def fetch_users():
+    cur = conn.cursor()
+    cur.execute("select * from users;")
+    for row in cur:
+        # print(row)
+        row = json.dumps(row, indent=4, sort_keys=True, default=str)
+        return(row)
+
+    # jsonitems = json.dump(cur.description)
+
+    cur.close()
+    conn.close()
+
+@app.route('/items')
 def fetch_items():
     cur = conn.cursor()
     cur.execute("select * from items;")
+    for row in cur:
+        # print(row)
+        row = json.dumps(row, indent=4, sort_keys=True, default=str)
+        return(row)
+
+    # jsonitems = json.dump(cur.description)
+
+    cur.close()
+    conn.close()
+
+@app.route('/orders')
+def fetch_orders():
+    cur = conn.cursor()
+    cur.execute("select * from ticket_orders;")
     for row in cur:
         # print(row)
         row = json.dumps(row, indent=4, sort_keys=True, default=str)
