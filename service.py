5850f9d9f45f7739babe9bef8f96607f287bd1c2
sans-agent <sans-agent@nsa.gov>
2020-11-14 00:25:11 -0600

    Adding new management pages

--- service.py
+++ service.py
@@ -29,6 +29,35 @@ def fetch_users():
     conn.close()
     return render_template("users.html", rows=dataset)
 
+@app.route('/manage/iteusersms', methods=["POST"])
+def manage_users():
+    conn = pymysql.connect(host=db_host, port=3306, user=db_user, passwd=db_pass, db=db_name)
+    cur = conn.cursor()
+    
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email_address = request.form['email_address']
+    phone_num = request.form['phone_num']
+    active  = request.form['active']
+    comments  = request.form['comments']
+    password  = request.form['password']
+    pass_salt  = request.form['pass_salt']
+    cur.execute(f'insert into users (first_name, last_name, email_address, phone_num, active, comments, password, pass_salt) values ("{first_name}", "{last_name}", "{email_address}", "{phone_num}", "{active}", "{comments}", "{password}", "{pass_salt}");')
+    res = conn.commit()
+
+    print(res)
+    
+    cur.close()
+    conn.close()
+
+    return '', 204
+ 
+@app.route('/manage/users', methods=["GET"])
+def get_manage_users():
+    return render_template("manage-users.html")
+
+
+
 # Fetch and Manage Items
 
 @app.route('/items')
@@ -67,7 +96,7 @@ def manage_items():
     return '', 204
  
 @app.route('/manage/items', methods=["GET"])
-def postget_manage_items():
+def get_manage_items():
     return render_template("manage-items.html")
 
 
@@ -78,7 +107,7 @@ def fetch_orders():
     conn = pymysql.connect(host=db_host, port=3306, user=db_user, passwd=db_pass, db=db_name)
     dataset = []
     cur = conn.cursor()
-    cur.execute("select * from ticket_orders;")
+    cur.execute("select * from orders;")
     results = cur.fetchall()
     for row in results:
         print(row)
@@ -86,7 +115,36 @@ def fetch_orders():
 
     cur.close()
     conn.close()
-    return render_template("items.html", rows=dataset)
+    return render_template("orders.html", rows=dataset)
+
+@app.route('/manage/orders', methods=["POST"])
+def manage_orders():
+    conn = pymysql.connect(host=db_host, port=3306, user=db_user, passwd=db_pass, db=db_name)
+    cur = conn.cursor()
+    
+    user_id = request.form['user_id']
+    ticket_ids = request.form['ticket_ids']
+    square_id = request.form['square_id']
+    paid = request.form['paid']
+    order_total = request.form['order_total']
+    order_complete = request.form['order_complete']
+    payment_status = request.form['payment_status']
+    cur.execute(f'insert into orders (user_id, ticket_ids, square_id, paid, order_total, order_complete, payment_status) values ("{user_id}, {ticket_ids}, {square_id}, {paid}, {order_total}, {order_complete}, {payment_status});')
+    res = conn.commit()
+
+    print(res)
+    
+    cur.close()
+    conn.close()
+
+    return '', 204
+
+@app.route('/manage/orders', methods=["GET"])
+def get_manage_orders():
+    return render_template("manage-orders.html")
+
+
+#Initialize Flask    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
