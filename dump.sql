0a8ae3f2720257bb24cf30fa5339b5667ed1b063
sans-agent <sans-agent@nsa.gov>
2020-11-16 17:07:49 -0600

    Update database Schema

--- dump.sql
+++ dump.sql
@@ -31,15 +31,15 @@ DROP TABLE IF EXISTS `items`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `items` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(15) NOT NULL AUTO_INCREMENT,
   `item_name` varchar(45) DEFAULT NULL,
-  `item_desc` varchar(350) DEFAULT NULL,
-  `item_price` varchar(15) DEFAULT NULL,
-  `sold` tinyint(4) DEFAULT NULL,
-  `creator` varchar(45) DEFAULT NULL,
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `item_desc` varchar(250) DEFAULT NULL,
+  `item_price` float DEFAULT NULL,
+  `available` int(1) DEFAULT NULL,
+  `created_by` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,40 +48,65 @@ CREATE TABLE `items` (
 
 LOCK TABLES `items` WRITE;
 /*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (0,'Global Ticket','One ticket to rule them all!','20.00',1,'bdorman','2020-11-05 00:18:35');
+INSERT INTO `items` VALUES (1,'Master Ticket','One ticket to rule them all!',20,1,'Bill Dorman','2020-11-11 08:15:42'),(2,'Master Ticket','One ticket to rule them all!',20,1,'Bill Dorman','2020-11-11 08:16:42'),(3,'Mountain Bike','Vroom',5,0,'Vanessa Dorman','2020-11-11 08:17:50');
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `ticket_orders`
+-- Table structure for table `orders`
 --
 
-DROP TABLE IF EXISTS `ticket_orders`;
+DROP TABLE IF EXISTS `orders`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ticket_orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ticket_number` bigint(20) DEFAULT NULL,
-  `item_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
-  `paid` tinyint(4) DEFAULT NULL,
-  `quantity` varchar(5) DEFAULT NULL,
-  `square_order` varchar(45) DEFAULT NULL,
-  `valid` tinyint(4) DEFAULT NULL,
-  `order_total` decimal(13,2) DEFAULT NULL,
-  `last_updated` datetime DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(20) DEFAULT NULL,
+  `ticket_ids` varchar(255) DEFAULT NULL,
+  `square_id` varchar(45) DEFAULT NULL,
+  `paid` int(1) DEFAULT NULL,
+  `order_total` float DEFAULT NULL,
+  `order_complete` int(1) DEFAULT NULL,
+  `payment_status` varchar(25) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='	';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ticket_orders`
+-- Dumping data for table `orders`
 --
 
-LOCK TABLES `ticket_orders` WRITE;
-/*!40000 ALTER TABLE `ticket_orders` DISABLE KEYS */;
-INSERT INTO `ticket_orders` VALUES (1,22154201,0,0,1,'5','213123',1,100.00,'2020-11-04 06:16:16');
-/*!40000 ALTER TABLE `ticket_orders` ENABLE KEYS */;
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tickets` (
+  `id` int(25) NOT NULL,
+  `ticket_number` int(20) DEFAULT NULL,
+  `user_id` int(20) DEFAULT NULL,
+  `item_id` int(20) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -92,14 +117,16 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` int(20) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email_address` varchar(128) DEFAULT NULL,
-  `phone_num` varchar(45) DEFAULT NULL,
-  `active` tinyint(4) DEFAULT NULL,
+  `phone_num` varchar(12) DEFAULT NULL,
+  `active` int(1) DEFAULT NULL,
   `comments` varchar(500) DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `password` varchar(128) DEFAULT NULL,
+  `pass_salt` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COMMENT='											';
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -110,7 +137,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (0,'Bill','Dorman','bill@2048-bit.com','4172079000',1,'Test User 1','2020-11-05 00:18:01'),(1,'Vanessa','Dorman','vanessabrill@gmail.com','4172079001',1,'Test User 2','2020-11-10 06:26:47');
+INSERT INTO `users` VALUES (0,'Bill','Dorman','bill@2048-bit.com','4172079000',1,'Test User 1','2020-11-05 00:18:01','e80b0c67e6fd0ccbeb50d914157ebc606ca470bb57f1ec704fe261091b2ac6cc','qw21s21ssdqw'),(1,'Vanessa','Dorman','vanessabrill@gmail.com','4172079001',1,'Test User 2','2020-11-10 06:26:47','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','23cew232323c2see12');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -123,4 +150,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-10 21:36:47
+-- Dump completed on 2020-11-16 23:06:54
