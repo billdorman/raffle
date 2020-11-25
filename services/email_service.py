4da95c078043875ffcd553c25c44e33bda7f4e1c
dev <dev@nsa.gov>
2020-11-25 01:52:48 -0600

    Fixing broken ass email service

--- services/email_service.py
+++ services/email_service.py
@@ -1,3 +1,4 @@
+import requests
 import logging
 import os
 from config import const as CONSTANTS
@@ -7,8 +8,8 @@ message_subject = 'Sweeny PTO - Raffle Information'
 
 def send_email(to_email, message_subject, message_body, from_email):
 	return requests.post(
-		EMAIL_API_URL,
-		auth=("api", EMAIL_API_KEY),
+		CONSTANTS.EMAIL_API_URL,
+		auth=("api", CONSTANTS.EMAIL_API_KEY),
 		data={"from": from_email,
 			"to": [to_email],
 			"subject": message_subject,
