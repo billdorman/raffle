893d602064e5d1018cfa3e71dbedb50e036c94f1
dev <dev@nsa.gov>
2020-11-25 01:34:31 -0600

    Adding email service

---
+++ services/email_service.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from config import const as CONSTANTS
+
+from_email = 'Sweeny PTO <donotreply@sweenypto.org>'
+message_subject = 'Sweeny PTO - Raffle Information'
+
+def send_email(to_email, message_subject, message_body, from_email):
+	return requests.post(
+		EMAIL_API_URL,
+		auth=("api", EMAIL_API_KEY),
+		data={"from": from_email",
+			"to": [to_email, to_email],
+			"subject": message_subject,
+			"text": message_body})
