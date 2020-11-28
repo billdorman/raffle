e556715bb206e311e5b6fbc48e206322b5aea949
Bill D <nexthoprouter@gmail.com>
2020-11-27 19:58:21 -0600

    Update const.py

--- config/const.py
+++ config/const.py
@@ -3,10 +3,10 @@ import os
 
 # Server Name
 SERVER_NAME = "RAFFLE_SERVER_01"
-BASE_URL = "http://localhost:5000"
+BASE_URL = "https://sweenypto.org"
 
-# SQUARE_API_URL = "connect.squareup.com"
-SQUARE_API_URL = "connect.squareupsandbox.com"
+SQUARE_API_URL = "connect.squareup.com"
+# SQUARE_API_URL = "connect.squareupsandbox.com"
 
 # Global Ticket
 GLOBAL_TICKET_ID = 1
